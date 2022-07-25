7fe9ce35e82c089cf3e2023530965e33b67cd635
Rabia <hnt.ozdmr@gmail.com>
2022-07-26 02:48:27 +0300

    userdata.sh changed

--- CLI_commands/userdata.sh
+++ CLI_commands/userdata.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-yum update -y
-yum install python3 -y
-pip3 install flask
+sudo yum update -y
+sudo yum install python3 -y
+sudo pip3 install flask
 cd /home/ec2-user
 wget -P templates https://raw.githubusercontent.com/OzdemirRabia/roman-numerals-converter/main/templates/index.html
 wget -P templates https://raw.githubusercontent.com/OzdemirRabia/roman-numerals-converter/main/templates/result.html
 wget https://raw.githubusercontent.com/OzdemirRabia/roman-numerals-converter/main/app.py
-python3 app.py
+sudo python3 app.py
